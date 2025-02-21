65ef568ab7aa2bc549cf60d4c7453aea79460e7c
David Pozo <david.pozo.plumed@gmail.com>
2025-02-21 11:01:50 +0100

    Añado test división

---
+++ src/test/java/calculadora/DivisionTest.java
@@ -0,0 +1,34 @@
+package calculadora;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DivisionTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void menuDivision() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void procesarOpcion1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void procesarOpcion2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void procesarOpcion3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calcularRaiz4() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void pintarMenu() {
+    }
+}
